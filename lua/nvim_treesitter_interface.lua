cab82939ef91fb43610b1cdd04f27cc4f063a9f0
michaelb <michael.bleuez2@gmail.com>
2020-09-23 22:22:20 +0200

    gg

--- lua/nvim_treesitter_interface.lua
+++ lua/nvim_treesitter_interface.lua
@@ -30,8 +30,8 @@ function M.list_nodes_in_range(start_row, end_row, bufnr)
   for _,node in ipairs(query_module.get_capture_matches(bufnr,"@function","code_deps")) do
     print("node found:", node.node)
     print("node's name:" , ts_utils.get_node_text(node.node)[1])
-    -- local sr,sc,er,ec = M.get_definition_scope_of_function_node(node.node,bufnr)
-    -- print(sr,sc,er,ec)
+    local sr,sc,er,ec = M.get_definition_scope_of_function_node(node.node,bufnr)
+    print(sr,sc,er,ec)
   end
 
 end
